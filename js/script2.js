aa5132e2985fe2f2a1d17738c17cb945bfdc182e
shahirsizan <shahir.sizan18@gmail.com>
2024-03-27 09:50:19 +0600

    home-background partial code added

--- js/script2.js
+++ js/script2.js
@@ -49,4 +49,40 @@ document.querySelector("#close-cart").onclick = () => {
 
 window.onscroll = () => {
 	navbar.classList.remove("active");
+	myOrders.classList.remove("active");
+	cart.classList.remove("active");
+};
+// End of Show/hide functionality for all side sections
+// End of Show/hide functionality for all side sections
+// End of Show/hide functionality for all side sections
+// End of Show/hide functionality for all side sections
+// End of Show/hide functionality for all side sections
+
+let slides = document.querySelectorAll(
+	".home-bg .home .slide-container .slide"
+);
+let index = 0;
+
+function next() {
+	slides[index].classList.remove("active");
+	index = (index + 1) % slides.length;
+	slides[index].classList.add("active");
+}
+
+function prev() {
+	slides[index].classList.remove("active");
+	index = (index - 1 + slides.length) % slides.length;
+	slides[index].classList.add("active");
+}
+// End of slides section scroll functionality
+// End of slides section scroll functionality
+// End of slides section scroll functionality
+// End of slides section scroll functionality
+// End of slides section scroll functionality
+
+
+
+
+window.onscroll = () => {
+	navbar.classList.remove("active");
 };
