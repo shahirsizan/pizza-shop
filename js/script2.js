35580946b75241f7034947444ac6946f83f8a94d
shahirsizan <shahir.sizan18@gmail.com>
2024-03-29 12:12:24 +0600

    html and css code for FAQ and Footer added

--- js/script2.js
+++ js/script2.js
@@ -80,9 +80,13 @@ function prev() {
 // End of slides section scroll functionality
 // End of slides section scroll functionality
 
+let accordion = document.querySelectorAll(
+	".faq .accordion-container .accordion"
+);
 
-
-
-window.onscroll = () => {
-	navbar.classList.remove("active");
-};
+accordion.forEach((acco) => {
+	acco.onclick = () => {
+		accordion.forEach((remove) => remove.classList.remove("active"));
+		acco.classList.add("active");
+	};
+});
